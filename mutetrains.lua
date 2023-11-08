6f3c1269da27eea671a43f860c979b7a3a04af60
Paul A. Cordova <57051068+pacordova@users.noreply.github.com>
2023-11-08 15:14:18 -0500

    Delete mutetrains.lua

--- mutetrains.lua
+++
@@ -1,9 +0,0 @@
---mute trains
-mutetrains = CreateFrame("FRAME")
-mutetrains:RegisterEvent("PLAYER_LOGIN");
-mutetrains:SetScript('OnEvent', function()
-	MuteSoundFile(539802)
-	MuteSoundFile(539881)
-	MuteSoundFile(540271)
-	MuteSoundFile(540275)
-end)

7a58972ab6b8c37be0c5a8c77c5f5e498bed9035
pac <pac3nc@virginia.edu>
2025-08-11 00:38:35 -0400

    merge

--- misc.lua
+++ misc.lua
@@ -17,6 +17,24 @@
 -- OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 -- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+--SetCVAR("gxWindowedResolution", "1920x1080")
+SetCVar("UIScale", 768/1080)
+SetCVar("cameraDistanceMaxZoomFactor", 1.5)
+DAMAGE_TEXT_FONT = "Fonts\\PEPSI.ttf"
+
+local main = CreateFrame('Frame');
+main:RegisterEvent('PLAYER_LOGIN');
+main:SetScript('OnEvent', function()
+   PlayerFrame:ClearAllPoints()
+   PlayerFrame:SetPoint("CENTER", UIParent, "CENTER", -500, 300)
+   PlayerFrame:SetUserPlaced(true);
+   TargetFrame:ClearAllPoints() 
+   TargetFrame:SetPoint("CENTER", UIParent, "CENTER", -250, 300) 
+   TargetFrame:SetUserPlaced(true)
+   FriendsMicroButton:Hide()
+   ChatFrameChannelButton:Hide()
+end)
+
 -- Volume Slider
 local vol = CreateFrame("Slider", "CharVol", PaperDollItemsFrame, "OptionsSliderTemplate")
 vol:SetWidth(75)

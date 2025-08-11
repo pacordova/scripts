1cf3d5365d0334d616571524799775f0e04fe287
pac <pac3nc@virginia.edu>
2025-08-11 00:34:26 -0400

    merge

--- formatfix.lua
+++ formatfix.lua
@@ -1,6 +1,6 @@
 -- MIT License
 
--- Copyright (c) 2020 Olle Månsson
+-- Copyright (c) 2020 Olle Månsson, draon12
 
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
@@ -55,3 +55,53 @@ hooksecurefunc("TextStatusBar_UpdateTextStringWithValues",function(self,...)
 		end
 	end
 end);
+
+---------------------------------------------
+
+local ADDON_NAME = ...
+
+
+---------------------------------------------
+-- HealthBar
+---------------------------------------------
+TargetFrameTextureFrame:CreateFontString("TargetFrameHealthBarText", "BORDER", "TextStatusBarText")
+TargetFrameHealthBarText:SetPoint("CENTER", TargetFrameTextureFrame, "CENTER", -50, 3)
+
+TargetFrameTextureFrame:CreateFontString("TargetFrameHealthBarTextLeft", "BORDER", "TextStatusBarText")
+TargetFrameHealthBarTextLeft:SetPoint("LEFT", TargetFrameTextureFrame, "LEFT", 8, 3)
+
+TargetFrameTextureFrame:CreateFontString("TargetFrameHealthBarTextRight", "BORDER", "TextStatusBarText")
+TargetFrameHealthBarTextRight:SetPoint("RIGHT", TargetFrameTextureFrame, "RIGHT", -110, 3)
+
+
+---------------------------------------------
+-- ManaBar
+---------------------------------------------
+TargetFrameTextureFrame:CreateFontString("TargetFrameManaBarText", "BORDER", "TextStatusBarText")
+TargetFrameManaBarText:SetPoint("CENTER", TargetFrameTextureFrame, "CENTER", -50, -8)
+
+TargetFrameTextureFrame:CreateFontString("TargetFrameManaBarTextLeft", "BORDER", "TextStatusBarText")
+TargetFrameManaBarTextLeft:SetPoint("LEFT", TargetFrameTextureFrame, "LEFT", 8, -8)
+
+TargetFrameTextureFrame:CreateFontString("TargetFrameManaBarTextRight", "BORDER", "TextStatusBarText")
+TargetFrameManaBarTextRight:SetPoint("RIGHT", TargetFrameTextureFrame, "RIGHT", -110, -8)
+
+
+---------------------------------------------
+-- INITIALIZE
+---------------------------------------------
+local frame = CreateFrame("frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:SetScript("OnEvent", function(self, event, addonName)
+    if ( event == "ADDON_LOADED" and addonName == ADDON_NAME ) then
+        TargetHealthDB = TargetHealthDB or { version=1, forcePercentages=false }
+
+        TargetFrameHealthBar.LeftText = TargetFrameHealthBarTextLeft;
+        TargetFrameHealthBar.RightText = TargetFrameHealthBarTextRight
+        TargetFrameManaBar.LeftText = TargetFrameManaBarTextLeft;
+        TargetFrameManaBar.RightText = TargetFrameManaBarTextRight;
+
+        UnitFrameHealthBar_Initialize("target", TargetFrameHealthBar, TargetFrameHealthBarText, true);
+        UnitFrameManaBar_Initialize("target", TargetFrameManaBar, TargetFrameManaBarText, true);
+    end
+end)
